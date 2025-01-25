c9d5913f92bf14309ca8d2517eb7b78e23fbbe3f
N-Determine <sreted@outlook.com>
2025-01-25 19:46:47 +0100

    fixed bug in which it was possible to delete the prompt after writing something

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 16:02:37 by adeters           #+#    #+#             */
-/*   Updated: 2025/01/25 17:34:46 by adeters          ###   ########.fr       */
+/*   Updated: 2025/01/25 19:45:02 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ char	*get_input(t_data *data)
 	if (SHOW_FOLDER && get_pwd(cwd, true))
 		return (NULL);
 	if (SHOW_FOLDER && data->exit_status)
-		printf("%s%s  %s%s%s ", FAIL_COLOR, PROMPT, CWD_COLOR, cwd, RESET);
+		printf("%s%s  %s%s ", FAIL_COLOR, PROMPT, CWD_COLOR, cwd);
 	else if (SHOW_FOLDER)
-		printf("%s%s  %s%s%s ", SUCC_COLOR, PROMPT, CWD_COLOR, cwd, RESET);
+		printf("%s%s  %s%s ", SUCC_COLOR, PROMPT, CWD_COLOR, cwd);
 	else if (data->exit_status)
-		printf("%s%s%s  ", FAIL_COLOR, PROMPT, RESET);
+		printf("%s%s  ", FAIL_COLOR, PROMPT);
 	else
-		printf("%s%s%s  ", SUCC_COLOR, PROMPT, RESET);
-	input = readline(NULL);
+		printf("%s%s  ", SUCC_COLOR, PROMPT);
+	input = readline(RESET);
 	return (input);
 }
