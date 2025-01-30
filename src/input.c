6d7981f25ac1ec42d08d513afbebe8130a96d48a
N-Determine <sreted@outlook.com>
2025-01-30 16:25:56 +0100

    made the input function a bit cleaner

--- src/input.c
+++ src/input.c
@@ -3,33 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   input.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsenniko <vsenniko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 16:02:37 by adeters           #+#    #+#             */
-/*   Updated: 2025/01/29 14:29:23 by vsenniko         ###   ########.fr       */
+/*   Updated: 2025/01/30 16:25:20 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*get_input(t_data *data)
+void	p_prompt(t_data *data)
+{
+	if (data->exit_status)
+		printf("%s%s  ", FAIL_COLOR, PROMPT);
+	printf("%s%s  ", SUCC_COLOR, PROMPT);
+}
+
+int	p_folder(void)
 {
 	char	cwd[PATH_MAX];
+	
+	if (SHOW_FOLDER && get_pwd(cwd, true))
+		return (1);
+	printf("%s%s ", CWD_COLOR, cwd);
+	return (0);
+}
+
+char	*get_input(t_data *data)
+{
 	char	*input;
 
-	if (SHOW_FOLDER && get_pwd(cwd, true))
+	p_prompt(data);
+	if (SHOW_FOLDER && p_folder())
 		return (NULL);
-	if (SHOW_FOLDER && data->exit_status)
-		printf("%s%s  %s%s ", FAIL_COLOR, PROMPT, CWD_COLOR, cwd);
-	else if (SHOW_FOLDER)
-		printf("%s%s  %s%s ", SUCC_COLOR, PROMPT, CWD_COLOR, cwd);
-	else if (data->exit_status)
-		printf("%s%s  ", FAIL_COLOR, PROMPT);
-	else
-		printf("%s%s  ", SUCC_COLOR, PROMPT);
-	// Add the git thing by checking for the .git folder
-	// (also check all parent folders/maybe recursively)
-	// Branch can be found within ./git/HEAD
 	input = readline(RESET);
 	return (input);
 }
--- src/main.c
+++ src/main.c
@@ -6,50 +6,44 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:56:57 by adeters           #+#    #+#             */
-/*   Updated: 2025/01/30 16:03:08 by adeters          ###   ########.fr       */
+/*   Updated: 2025/01/30 16:24:59 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	main(void)
-// {
-// 	t_data	data;
-
-// 	if (init_shell(&data))
-// 		return (p_err(INIT));
-	
-// 	while (true)
-// 	{
-// 		data.input = get_input(&data);
-// 		// Should use vova's prompt (because stuff like "cl"ea"r")
-// 		if (!ft_strncmp(data.input, "clear", 5))
-// 			clear();
-// 		free(data.input);
-// 	}
-// 	while (1)
-// 	{
-// 		char *line = readline("minishell>");
-// 		printf("Recieved:\n");
-// 		printf("%s\n", line);
-// 	}
-// }
-
-int	main(int argc, char **argv, char **env)
+int	main(void)
 {
-	t_data		data;
-	t_env_lst	*node;
+	t_data	data;
 
-	(void)argc;
-	(void)argv;
 	if (init_shell(&data))
 		return (p_err(INIT));
-	parse_env(&data, env);
-	node = data.env_lst;
-	while (node)
+	
+	while (true)
 	{
-		printf("%s=%s\n", node->filed, node->value);
-		node = node->next;
+		data.input = get_input(&data);
+		// Should use vova's prompt (because stuff like "cl"ea"r")
+		if (!ft_strncmp(data.input, "clear", 5))
+			clear();
+		free(data.input);
 	}
-	ft_env_lstclear(&data.env_lst);
 }
+
+// int	main(int argc, char **argv, char **env)
+// {
+// 	t_data		data;
+// 	t_env_lst	*node;
+
+// 	(void)argc;
+// 	(void)argv;
+// 	if (init_shell(&data))
+// 		return (p_err(INIT));
+// 	parse_env(&data, env);
+// 	node = data.env_lst;
+// 	while (node)
+// 	{
+// 		printf("%s=%s\n", node->filed, node->value);
+// 		node = node->next;
+// 	}
+// 	ft_env_lstclear(&data.env_lst);
+// }
