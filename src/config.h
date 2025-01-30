8bef623ba0fef8bebbf5cb6872c32e10e0fedc2c
N-Determine <sreted@outlook.com>
2025-01-30 16:39:56 +0100

    start the show git function

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 16:48:38 by adeters           #+#    #+#             */
-/*   Updated: 2025/01/25 16:50:31 by adeters          ###   ########.fr       */
+/*   Updated: 2025/01/30 16:36:18 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@
 # define CWD_COLOR "\033[1;36m"
 # define RESET "\033[0m"
 
+# define GIT_COLOR "\033[1;36m"
+# define GIT_COLOR_BRANCH "\033[1;91m"
+
 # ifndef PROMPT
 #  define PROMPT "➜"
 # endif
 
 # ifndef SHOW_FOLDER
 #  define SHOW_FOLDER true
+# endif
+
+# ifndef SHOW_GIT
+#  define SHOW_GIT true
 # endif
 
 #endif
