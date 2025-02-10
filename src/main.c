21b3627f6b797b2b7d2d036d87a44005c2fe90dc
N-Determine <sreted@outlook.com>
2025-02-10 20:05:04 +0100

    added new main

--- src/main.c
+++ src/main.c
@@ -3,34 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsenniko <vsenniko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:56:57 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/10 20:01:35 by vsenniko         ###   ########.fr       */
+/*   Updated: 2025/02/10 20:04:52 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	main(void)
-// {
-// 	t_data	data;
+int	main(void)
+{
+	t_data	data;
 
-// 	if (init_shell(&data))
-// 		return (p_err(INIT));
-	
-// 	while (true)
-// 	{
-// 		data.input = get_input(&data);
-// 		// Should use vova's prompt (because stuff like "cl"ea"r")
-// 		if (!ft_strncmp(data.input, "clear", 5))
-// 			clear();
-// 		free(data.input);
-// 	}
-// 	// while (1)
-// 	// {
-// 	// 	char *line = readline("minishell>");
-// 	// 	printf("Recieved:\n");
-// 	// 	printf("%s\n", line);
-// 	// }
-// }
+	if (init_shell(&data))
+		return (p_err(INIT));
+	while (true)
+	{
+		data.input = get_input(&data);
+		if (data.input && !ft_strncmp(data.input, "clear", 5))
+			clear();
+		else if (data.input && !ft_strncmp(data.input, "pwd", 3))
+			pwd();
+		free(data.input);
+	}
+}
