ac3d4c150b6f74893043cd1dc5a37b958a94addd
Volodymyr Sennikov <vsenniko@c3r7p3.42vienna.com>
2025-02-11 14:12:01 +0100

    improoved lexing. Properly handling $, >>, <<, quotes (single and double)

--- src/lexing.c
+++ src/lexing.c
@@ -6,7 +6,7 @@
 /*   By: vsenniko <vsenniko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:54:11 by vsenniko          #+#    #+#             */
-/*   Updated: 2025/02/10 19:54:39 by vsenniko         ###   ########.fr       */
+/*   Updated: 2025/02/11 14:07:19 by vsenniko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int	lexing(char *input, t_token **list)
 		while (input[i] && ft_is_space(input[i]))
 			i++;
 		if (!input[i])
-			return (0);
+			return (1);
 		if (!process_token(input, &current, &i))
 			return (0);
 		ft_token_lstadd_back(list, current);
--- src/lexing_utils_3.c
+++ src/lexing_utils_3.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   helpers2.c                                         :+:      :+:    :+:   */
+/*   lexing_utils_3.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsenniko <vsenniko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/10 19:58:11 by vsenniko          #+#    #+#             */
-/*   Updated: 2025/02/10 19:58:47 by vsenniko         ###   ########.fr       */
+/*   Created: 2025/02/11 13:24:53 by vsenniko          #+#    #+#             */
+/*   Updated: 2025/02/11 14:08:21 by vsenniko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,20 @@ int	ft_is_space(char ch)
 		|| ch == '\r')
 		return (1);
 	return (0);
+}
+
+int	handle_dolar(char *input, t_token **current, int *i, char **word)
+{
+	int	j;
+
+	j = ++(*i);
+	while (input[*i] && !ft_is_space(input[*i]) && input[*i + 1] != '$')
+		(*i)++;
+	*word = ft_substr(input, j, *i - j + 1);
+	if (ft_strnstr(*word, "|><", ft_strlen(*word)))
+		return (free(*word), 0);
+	if (!*word)
+		return (*word = NULL, 0);
+	*current = create_token(DOLAR_SIGN, *word);
+	return (1);
 }
