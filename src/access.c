99783ca8926699e14f53f51216dde5caa09df653
atdeters <sreted@outlook.com>
2025-02-19 15:41:26 +0100

    broken up the get_fds function for nn

--- src/access.c
+++ src/access.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:53:22 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/19 15:11:40 by adeters          ###   ########.fr       */
+/*   Updated: 2025/02/19 15:33:50 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,28 @@ int	check_access(t_data *data, char *name, bool is_file)
 		error = p_err_arg(ERR_ACCESS, name);
 	if (error)
 		return (setnret(data, error));
+	return (0);
+}
+
+int	check_access_files(t_data *data)
+{
+	int	out_m;
+	int	in_m;
+	int	error;
+
+	out_m = data->parsed_lst->out_mode;
+	in_m = data->parsed_lst->in_mode;
+	if (in_m == IN_MODE_FILE)
+	{
+		check_access (data, data->parsed_lst->in, true);
+		if (error)
+			return (setnret(data, error));
+	}
+	if (out_m == OUT_MODE_FILE_APP || out_m == OUT_MODE_FILE_TR)
+	{
+		check_access (data, data->parsed_lst->out, true);
+		if (error)
+			return (setnret(data, error));
+	}
 	return (0);
 }
