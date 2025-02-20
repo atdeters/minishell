65bf6a4e85c6a6f4d83deb893051c525df369f39
atdeters <sreted@outlook.com>
2025-02-20 22:13:13 +0100

    added necessary commentary

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,12 +6,13 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 21:57:21 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/20 21:59:53 by adeters          ###   ########.fr       */
+/*   Updated: 2025/02/20 22:13:03 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// Not fully correct (doesn't include newly added stuff)
 void	ft_env(t_data *data)
 {
 	ft_echo(data->original_env);
