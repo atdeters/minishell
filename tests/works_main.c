5ccbb2d93e38d7d91992ac9ec5b1667002f1e6c7
atdeters <sreted@outlook.com>
2025-02-22 19:40:27 +0100

    got rid of limitations file

--- tests/works_main.c
+++ tests/works_main.c
@@ -6,11 +6,29 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:56:57 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/22 19:23:37 by adeters          ###   ########.fr       */
+/*   Updated: 2025/02/22 19:39:25 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../src/minishell.h"
+
+int handle_prompt(t_data *data, char **av);
+
+int	main(int ac, char **av, char **env)
+{
+	t_data	data;
+
+	if (init_shell(&data, ac, av, env))
+		return (pc_err(data.error));
+	while (data.on)
+	{
+		if (handle_prompt(&data, av))
+			rage_quit(&data, data.error);
+		if (data.flag_single)
+			data.on = false;
+	}
+	rage_quit(&data, 0);
+}
 
 int	handle_prompt(t_data *data, char **av)
 {
@@ -27,7 +45,8 @@ int	handle_prompt(t_data *data, char **av)
 		pnc_err(data);
 	if (!parser_main(&data->token_lst, data))
 		pnc_err(data);
-	pipe_maker(data);
+	if (pipe_maker(data))
+		pnc_err(data);
 	while (data->parsed_lst)
 	{
 		execute(data);
@@ -38,20 +57,4 @@ int	handle_prompt(t_data *data, char **av)
 	if (!data->flag_single)
 		free(data->input);
 	return (0);
-}
-
-int	main(int ac, char **av, char **env)
-{
-	t_data	data;
-
-	if (init_shell(&data, ac, av, env))
-		return (pc_err(data.error));
-	while (data.on)
-	{
-		if (handle_prompt(&data, av))
-			rage_quit(&data, data.error);
-		if (data.flag_single)
-			data.on = false;
-	}
-	rage_quit(&data, 0);
 }
