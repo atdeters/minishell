9c428ecd493878500e0b38c1b727f6c0220dadeb
atdeters <sreted@outlook.com>
2025-02-23 17:51:58 +0100

    added necessary commentary

--- src/aliases.c
+++ src/aliases.c
@@ -6,9 +6,12 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 17:45:36 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/23 17:48:01 by adeters          ###   ########.fr       */
+/*   Updated: 2025/02/23 17:51:50 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// Use vovas functions to parse the .vash_alias file and create a list
+// make it a builtin function that can show the list and add entry just like
+// the export function
