68ba24c4a9236fff3c73c787944597be02af3762
atdeters <sreted@outlook.com>
2025-02-27 19:59:51 +0100

    redone as it should be

--- tests/works_main.c
+++ tests/works_main.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:56:57 by adeters           #+#    #+#             */
-/*   Updated: 2025/02/27 19:51:17 by adeters          ###   ########.fr       */
+/*   Updated: 2025/02/27 19:59:35 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,6 @@ int	handle_prompt(t_data *data, char **av)
 		pnc_err(data);
 	if (fill_hdf_arr(data, &data->token_lst))
 		pnc_err(data);
-
-
-
-
-
 	if (parser_main(data))
 		pnc_err(data);
 	if (pipe_maker(data))
