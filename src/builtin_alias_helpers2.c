e050727303b2e0cc4b8071b36b2259fd2411b61b
N-Determine <sreted@outlook.com>
2025-03-09 12:37:33 +0100

    added feature to overwrite already existing aliases while asking the user if that should be done

--- src/builtin_alias_helpers2.c
+++ src/builtin_alias_helpers2.c
@@ -6,7 +6,7 @@
 /*   By: andreas <andreas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:24:14 by adeters           #+#    #+#             */
-/*   Updated: 2025/03/09 11:57:20 by andreas          ###   ########.fr       */
+/*   Updated: 2025/03/09 12:36:13 by andreas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,12 +88,65 @@ bool	is_unique_key(t_env_lst *lst, char *entry)
 	}
 	check = get_value_from_lst(lst, key);
 	if (!ft_strcmp(check, key))
-		return (free(key), true);
-	return (free(key), false);
+		return (free(key), free(check), true);
+	return (free(key), free(check), false);
+}
+
+bool	input_is_yes(char *input)
+{
+	if (!ft_strcmp(input, "yes\n"))
+		return (true);
+	if (!ft_strcmp(input, "Yes\n"))
+		return (true);
+	if (!ft_strcmp(input, "y\n"))
+		return (true);
+	if (!ft_strcmp(input, "Y\n"))
+		return (true);
+	if (!ft_strcmp(input, "\n"))
+		return (true);
+	return (false);
+}
+
+bool	input_is_no(char *input)
+{
+	if (!ft_strcmp(input, "no\n"))
+		return (true);
+	if (!ft_strcmp(input, "No\n"))
+		return (true);
+	if (!ft_strcmp(input, "n\n"))
+		return (true);
+	if (!ft_strcmp(input, "N\n"))
+		return (true);
+	return (false);
+}
+
+bool	user_agrees(t_data *data)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	if (!line)
+		rage_quit(data, ERR_MALLOC, true, NULL);
+	while (line)
+	{
+		if (input_is_yes(line))
+			return (free(line), true);
+		if (input_is_no(line))
+			return (free(line), false);
+		ft_printf("Invalid input. Please enter Y or n ");
+		free(line);
+		line = get_next_line(0);
+		if (!line)
+			rage_quit(data, ERR_MALLOC, true, NULL);
+	}
+	return (free(line), false);
 }
 
 int	check_entry(t_data *data, char *entry)
 {
+	int		eq_ind;
+	char	*dupe;
+
 	if (!is_valid_entry_form(entry))
 	{
 		printf("Not a valid entry for alias\n");
@@ -101,10 +154,23 @@ int	check_entry(t_data *data, char *entry)
 	}
 	if (!is_unique_key(data->alias_lst, entry))
 	{
+		// ! Make this print to stderr
 		ft_printf("\"%s\" is not a unique entry.\n", entry);
-		ft_printf("Choose another one or remove this one ");
-		ft_printf("by using \"alias -r [key]\"\n");
-		return (setnret(data, ERR_DUP_ENTRY));
+		ft_printf("Do you want to overwrite it? [Y/n] ");
+		if (user_agrees(data))
+		{
+			if (ft_strchr(entry, '='))
+			{
+				eq_ind = ft_strchr(entry, '=') - entry;
+				dupe = ft_strdup(entry);
+				if (!dupe)
+					rage_quit(data, ERR_MALLOC, true, NULL);
+				dupe[eq_ind] = '\0';
+			}
+			return (remove_alias(&data->alias_lst, dupe), free(dupe), 0);
+		}
+		else
+			return (setnret(data, ERR_DUP_ENTRY));
 	}
 	return (0);
 }
