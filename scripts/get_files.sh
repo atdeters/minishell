38236fb7241234f06ebabcf9ac495f659f943730
atdeters <sreted@outlook.com>
2025-05-06 13:45:06 +0200

    chore: Delete scripting folder

--- scripts/get_files.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Variables
-SRC='src'			# directory with your files you want to sort
-FPL='1'				# number of files per line
-OUT='files_sorted'	# filename for the output of the sorted files
-
-# Script
-ls $SRC | grep .c | awk -v n=$FPL '{printf "%s ", $0; if (NR % n == 0) print "\\"}' > $OUT
